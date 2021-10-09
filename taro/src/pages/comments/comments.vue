7e63029a83eaa6d785d79339e356f8f26d754036
korilin <korilin.dev@gmail.com>
2021-10-09 23:09:37 +0800

    comment 布局

--- taro/src/pages/comments/comments.vue
+++ taro/src/pages/comments/comments.vue
@@ -1,21 +1,49 @@
 <template>
     <view class="comments">
         <view>
-            发表你的评论:
-            <nut-textarea
-                v-if="isLogin"
-                rows="2"
-                limit-show
-                max-length="200"
-                v-model="content"
-            />
-            <nut-button v-else type="info">请先登录</nut-button>
+            <view style="font-size: 14px; margin-bottom: 10px">
+                发表你的评论：
+            </view>
+            <view style="text-align: center; width: 100%">
+                <nut-textarea
+                    v-if="isLogin"
+                    rows="2"
+                    limit-show
+                    max-length="200"
+                    v-model="content"
+                />
+                <nut-button v-else type="info">请先登录</nut-button>
+            </view>
+        </view>
+        <nut-divider style="margin: 15px 0">大家的留言</nut-divider>
+        <view>
+            <view v-for="(comment, i) in comments" :key="comment.commentId">
+                <view class="comment-wrap">
+                    <view class="info">
+                        <nut-avatar size="40" :icon="comment.avatarUrl" />
+                    </view>
+                    <view class="content">
+                        <view class="nick-name">{{ comment.nickName }}</view>
+                        <view class="time">{{
+                            moment(comment.timestamp).format(
+                                "YYYY/MM/DD HH:mm:ss"
+                            )
+                        }}</view>
+                        <nut-cell> {{ comment.content }} </nut-cell>
+                    </view>
+                </view>
+                <nut-divider dashed>
+                    {{ i == comments.length - 1 ? "到底了" : "⚪" }}
+                </nut-divider>
+            </view>
         </view>
     </view>
 </template>
 
 <script>
 import { reactive, toRefs } from "vue";
+import Taro from "@tarojs/taro";
+import moment from "moment";
 
 export default {
     name: "Comments",
@@ -25,6 +53,14 @@ export default {
             isLogin: false,
             comments: [
                 {
+                    commentId: 0,
+                    nickName: "kori",
+                    avatarUrl: "https://korilin.com/korilin.png",
+                    content: "你好呀",
+                    timestamp: 1633788029,
+                },
+                {
+                    commentId: 1,
                     nickName: "kori",
                     avatarUrl: "https://korilin.com/korilin.png",
                     content: "你好呀",
@@ -35,6 +71,7 @@ export default {
 
         return {
             ...toRefs(state),
+            moment,
         };
     },
 };
@@ -45,7 +82,33 @@ export default {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     padding: 5vh 5vh;
+}
+
+.comment-wrap {
+    display: flex;
+    width: 100%;
+    margin: 18px 0;
+
+    .info {
+        width: 40px;
+    }
+
+    .content {
+        width: 100%;
+        padding-left: 20px;
+
+        .nick-name {
+            color: black;
+            font-size: 16px;
+            margin-top: 5px;
+        }
+
+        .time {
+            color: #ccc;
+            font-size: 12px;
+            margin-top: 3px;
+        }
+    }
 }
 </style>
